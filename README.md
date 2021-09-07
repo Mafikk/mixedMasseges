f3e9ab2cddba0bd773f5e5a7f1bed355ab2ad8bd
Mafikk <90154390+Mafikk@users.noreply.github.com>
2021-09-07 18:36:00 +0200

    function added - log sentence

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # mixedMasseges
 Codecademy portfolio project
+
+## Goal of the project
+----------
+Goal of the project was to create an random message each time you run a program. 
+
+## Topic 
+-----------
+Based on random "You seem to have an ? day" I've replied either with a funny motivational quote or a simple joke. 
+
+## Used technologies
+----------
++ Git & GitHub
++ Node.js
++ JavaScript
+
+## Program structure
+-------------------
+Program is based on 4 different arrays and a fuction taking 2 as arguments. Function logs random greeting and a random sentecnce - You seem to have a bad day. Here's a joke. / You seem to have a lack of motivation. Here's a motivational quote. 
+
+Based on which of two random sentences fuction logs either a joke or an motivational quote. 
+
+## Lessons learned
+-------------------
+Project was from more oriented on getting used to your own dev enviroment, Git&GitHub and Node.js debugging than on JS skills. 
--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const firstPart = [];
-const secondPart = [];
-const thirdPart = [];
+const greeting = ['Hello', 'Good day', 'Yo'];
+const day = ['You seem to have a bad day, here\'s a joke', 'You seem to have a lack of motivation, here\'s a quote for you', ];
+const inspiration = ['Whatever you do, always give 100%. Unless you\'re donating blood.', 'Copcakes are muffins that believed in miracles.', 'If at first you don\'t suceed, than skydiving definately isn\t for you.'];
+const jokes = ['Two artists had an art contst. It ended in a draw.', 'How do you make a tissue dance? Put a little boogie in it.', 'I have a fear of speed bumps. But, I am slowly getting over it.'];
+
+
+let returnRandomSentence = (array1, array2) => {
+    let greet = array1[Math.floor(Math.random()*array1.length)];
+    let dayIndex = Math.floor(Math.random()* array2.length); 
+    console.log(`${greet}. ${array2[dayIndex]}`);
+
+    if (dayIndex === 1) {
+        console.log(inspiration[Math.floor(Math.random()*inspiration.length)]);
+    } else if (dayIndex === 0) {
+        console.log(jokes[Math.floor(Math.random()*jokes.length)])
+    };
+};
+
+returnRandomSentence(greeting, day);
